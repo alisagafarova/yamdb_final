1b4be0236953ac0e97ab7debfa2b2cdbef22876e
Алиса В. Гафарова <alisa.gafarova@gafarova-mp2.yamoney.ru>
2022-04-11 09:00:00 +0300

    docker_commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,10 +1,13 @@
 import os
 from datetime import date, timedelta
 
+from dotenv import find_dotenv, load_dotenv
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
@@ -58,15 +61,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv(
-            'DB_ENGINE',
-            default='django.db.backends.postgresql',
-        ),
-        'NAME': os.getenv('DB_NAME', default='postgres',),
-        'USER': os.getenv('POSTGRES_USER', default='postgres',),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres',),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432',),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
